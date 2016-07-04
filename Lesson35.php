b06c8c9e7a8bcbe8f5f33706ce0ffae8b8a453f2
androidhotspot <androooidhotspot@gmail.com>
2016-07-03 22:57:10 -0700

    Add files via upload

---
+++ Lesson35.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Include Function PHP Essential Training</title>
+</head>
+<body>
+<?php
+include 'include_functions.php';
+sample(50, 25);
+?>
+</body>
+</html>
